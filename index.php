7c224722b06600616cff06a54405cbffb85005b7
root <root@studvm59-p.cs.ucl.ac.uk>
2013-10-31 21:24:39 +0000

    Added new column 2

--- index.php
+++ index.php
@@ -45,13 +45,13 @@
         $companu = $_POST['Company'];
         $date = date("Y-m-d");
         // Insert data
-        $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
+        $sql_insert = "INSERT INTO registration_tbl (name, email,Company, date) 
                    VALUES (?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $date);
-        $stmt->bindValue(4, $Company);
+        $stmt->bindValue(3, $Company);
+        $stmt->bindValue(4, $date);
         $stmt->execute();
     }
     catch(Exception $e) {
